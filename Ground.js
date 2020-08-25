e642d3588597b9cd90e3a54652a2d113323488f1
Isa-Syed-08 <69919735+Isa-Syed-08@users.noreply.github.com>
2020-08-25 14:38:05 -0400

    Update Ground.js

--- Ground.js
+++ Ground.js
@@ -6,6 +6,8 @@ class Ground{
         this.body = Bodies.rectangle(x,y,width,height,options)
         this.width = width
         this.height = height
+        World.add(world,this.body)
+
 
     }
     display(){
